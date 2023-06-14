c0f70d5f7abe10bc76f477412685cfe3133634ac
Teeejaaa <dobterka@seznam.cz>
2023-06-14 09:17:28 +0200

    0,2

---
+++ downmenu.php
@@ -0,0 +1,20 @@
+
+<footer id="footer">
+
+  
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          
+        </div>
+        <div class="credits">
+        
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://www.facebook.com/SPSeiOstrava.cz" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/spsei_ostrava/" class="instagram"><i class="bx bxl-instagram"></i></a>
+      </div>
+    </div>
+  </footer>
--- index.php
+++ index.php
@@ -160,25 +160,10 @@ include "./menu.php";
   </main>
 
 
-  <footer id="footer">
-
+  <?php
+  include "./downmenu.php";
   
-    <div class="container d-md-flex py-4">
-
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          
-        </div>
-        <div class="credits">
-        
-        </div>
-      </div>
-      <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="https://www.facebook.com/SPSeiOstrava.cz" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="https://www.instagram.com/spsei_ostrava/" class="instagram"><i class="bx bxl-instagram"></i></a>
-      </div>
-    </div>
-  </footer>
+  ?>
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
